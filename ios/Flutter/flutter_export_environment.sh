23fac286e0746782d76519b51e53cf26f58fe29f
Carlos Reyes Munoz <carlos@finlabs.fi>
2019-12-12 15:41:20 +0200

    ios firebase configuration

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/carlosreyes/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/carlosreyes/gymcontrol"
-export "FLUTTER_TARGET=/Users/carlosreyes/gymcontrol/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/carlosreyes/gymcontrol/gymcontrol"
+export "FLUTTER_TARGET=/Users/carlosreyes/gymcontrol/gymcontrol/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/carlosreyes/development/flutter/bin/cache/artifacts/engine/ios"
