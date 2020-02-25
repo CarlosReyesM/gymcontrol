b03f4df8af2b675980e7dcc5ff8e0f00c237c6a2
CarlosReyesM <CarlosReyesM>
2020-02-25 20:00:31 +0200

    update status

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gymcontrol/services/auth.dart';
 import 'package:provider/provider.dart';
-import '../models/user.dart';
-import './wrapper.dart';
+import 'models/user.dart';
+import 'screens/wrapper.dart';
 
 
 void main() => runApp(MyApp());
